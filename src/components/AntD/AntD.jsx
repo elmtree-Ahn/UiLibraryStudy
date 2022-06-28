19ff6ccc4dc4201e782b8538b9f4a2b6adccfa69
elmtree-Ahn <clfclf3138@gmail.com>
2022-06-29 08:56:39 +0900

    start project

---
+++ src/components/AntD/AntD.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GetStart from './GetStart';
+
+const AntD = () => {
+    return (
+        <div>
+            <GetStart />
+        </div>
+    );
+};
+
+export default AntD;
